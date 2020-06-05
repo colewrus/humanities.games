010647695bc0e60c4cc8fdeb07785c9e88b03f1d
colewrus <wrampelc@gmail.com>
2020-06-05 16:38:49 -0400

    got a basic platformer made for walkthrough on phaser

--- games/platform/game.js
+++ games/platform/game.js
@@ -9,63 +9,116 @@ var config = {
     },      
     physics: {
         default: 'arcade', 
+        arcade: {
+            gravity: {y:300},
+            debug: true,
+        }
     },
     scene: {
         preload: preload,
         create: create,
         render: render,
         update: update        
-    }
+    },
+    backgroundColor: "#4488AA",
 };
 
-var greeble
-var destX = 0;
+
+var player;
+
+var platforms;
 var move = false;
 var target = new Phaser.Math.Vector2();
-
+var arrows; //arrow keys for keyboard control
+var win = false;
 
 var game = new Phaser.Game(config);
+var playerSpawn = new Phaser.Math.Vector2();
 
 function preload(){
     this.load.image('greeble', 'assets/vis/greeble.png');
     this.load.spritesheet('fireball', 'assets/vis/xfireball.png', {frameWidth: 32, frameHeight: 32});
-
+    this.load.image('ground', 'assets/vis/red32x32.png');
+    this.load.spritesheet('cole', 'assets/vis/cole-run-only.png', {frameWidth: 32, frameHeight: 32});
+    this.load.spritesheet('coinPNG', 'assets/vis/Coin.png', {frameWidth: 32, frameHeight: 32});
+    
+    //audio
+    this.load.audio('bkg', 'assets/audio/harp_bourree.mp3');
+    this.load.audio('win', 'assets/audio/trumpet_1.mp3');
 }
 
-function create(){
+function create(){       
+//AUDIO
+    var bkgMusic = this.sound.add('bkg', {volume: 0.5, loop: true});
+    bkgMusic.play();
 
-    text = this.add.text(10,10, '', {font: '11px Courier', fill: '#00ff00'});
-  
-    greeble = this.physics.add.image(400, 300, 'greeble');    
-    greeble.setBounce(0.2, 0.2);
-    greeble.setCollideWorldBounds(true);
+
+//PLATFORMS
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(0+32*4, this.game.canvas.height-16,'ground').setScale(8, 1).refreshBody();   
+    platforms.create(this.game.canvas.width-32*5, this.game.canvas.height-16, 'ground').setScale(10,1).refreshBody();
 
 //FIREBALL
     var fireball_config = {
         key: 'base',
         frames: this.anims.generateFrameNumbers('fireball', {end:4 }),
-        frameRate: 12,
+        frameRate: 12,        
         repeat: -1        
     }
- 
-
    fireball_anim = this.anims.create(fireball_config);
-
-   fireball = this.add.sprite(250, 250, 'fireball');
+   fireball = this.physics.add.sprite(300, this.game.canvas.height + 16, 'fireball');     
+   fireball.body.setAllowGravity(false);
+   fireball.body.setCircle(16,0,0);
+   fireball.angle = 90;
    fireball.anims.load('base'); 
    fireball.anims.play('base');
+   var fireballTween = this.tweens.add({
+       targets: fireball,
+       y: 220,
+       ease: 'Linear',
+       duration: 1500,
+       repeat: -1, 
+       yoyo: true,
+       flipX: true,
+   })
+//COIN
+  
+    var coin = this.physics.add.sprite(this.game.canvas.width-32, this.game.canvas.height - 75, 'coinPNG');
+    coin.body.setAllowGravity(false);
+    coin.body.setCircle(11,5,5);
+
+//PLAYER  
+   var player_config = {
+       key: 'run',
+       frames: this.anims.generateFrameNumbers('cole'),
+       frameRate: 12,
+       repeat: -1
+   }
+   player = this.physics.add.sprite(32, 270, 'cole');
+   player.setBounce(0.2);
+   player.setCollideWorldBounds(false);
+   player.body.setSize(18,32);
+
+   player_anim = this.anims.create(player_config);
+   player.anims.load('run');
+   player.anims.play('run');
+   playerSpawn.set(32, 270);
 
 
-//POINTER
-    this.input.on('pointerdown', function(pointer){//pointer control
-        target.x = pointer.x;
-        target.y = pointer.y;
+//Controls
+   arrows = this.input.keyboard.createCursorKeys();
 
-        // this.physics.moveToObject(greeble, target, 200);
-        move = true;   
-    }, this);
+//Physics
+
+   this.physics.add.collider(player, platforms);
+   this.physics.add.overlap(player, fireball, fireballTouch, null, this);
+   this.physics.add.overlap(player, coin, setWin, null, this);
 
 }
+
+// ----END OF CREATE
 
 function render(){
     game.debug.inputInfo(32, 32);
@@ -74,35 +127,52 @@ function render(){
 
 
 function update(){
+    
     var p = this.input.activePointer;
 
-    var distance = Phaser.Math.Distance.Between(greeble.x, greeble.y, target.x, target.y); 
-    
-    text.setText([
-        'x: ' + target.x,
-        'y: ' + target.y,   
-        'distance: ' + distance
-    ]);
-
-    target.x = p.x;
-    target.y = p.y;
-    this.physics.moveToObject(greeble, target, 200);
-    
-    if(distance < 4){
-        greeble.body.stop();
-        console.log("should stop");
-        move = false;
-    }       
-
-    // if(move){       
-    //     if(distance < 4){
-    //         greeble.body.stop();
-    //         console.log("should stop");
-    //         move = false;
-    //     }       
-    // }
-    
+    if(arrows.left.isDown){
+        player.setVelocityX(-160);
+        player.flipX = true;
+    }else if(arrows.right.isDown){
+        player.setVelocityX(160);
+        player.flipX = false;   
+    }else{
+        player.setVelocityX(0);
+    }
+  
+    if(arrows.up.isDown && player.body.touching.down){   
+        player.setVelocityY(-150);
+    }
+    checkPlayerOOB(player); 
+    //console.log("Player bounds " , player.getBounds().y);
 }
 
+function checkPlayerOOB(p){
+    if(p.getBounds().y > this.game.canvas.height){
+        console.log("Player off screen");
+        p.setPosition(playerSpawn.x, playerSpawn.y);
+    }
+}
 
+function respawnPlayer(){
+    player.setPosition(playerSpawn.x, playerSpawn.y);
+}
+
+function flipSprite(_sprite){
+    console.log(_sprite.angle);
+    _sprite.angle = _sprite.angle * -1;
+}
+
+function fireballTouch(){
+    respawnPlayer();
+}
+
+function setWin(){
+    if(!win){
+        var tex = this.add.text(300, 50, "You Win");
+        win = true;        
+        var winSound = this.sound.add('win', {volume: 0.75});
+        winSound.play();
+    }
+}
 
