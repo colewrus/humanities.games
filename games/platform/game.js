e2ef4cd78472a079ae04b8616f5f799902cbb349
colewrus <wrampelc@gmail.com>
2020-06-16 16:27:48 -0400

    return playable control, added globe theater and old london bridge to map

--- games/platform/game.js
+++ games/platform/game.js
@@ -46,6 +46,7 @@ var self = this;
 var jumpTimer;
 var debugText;
 
+var worldHeight;
 
 function preload(){
     this.load.image('greeble', 'assets/vis/greeble.png');
@@ -56,17 +57,14 @@ function preload(){
     
     //audio
     this.load.audio('bkg', 'assets/audio/harp_bourree.mp3');
+    this.load.audio('win', 'assets/audio/trumpet_1.mp3');
 
     //UI
     this.load.image('arrow', 'assets/vis/arrow.png');
 
     //Shakespeare
-    this.load.image('globe', 'assets/vis/globe-drawn.jpg');
-
-    this.load.audio('win', 'assets/audio/collect-4.mp3');
-    this.load.audio('loss', 'assets/audio/loss-1.mp3'); 
-    this.load.audio('jump', 'assets/audio/jump-1.mp3');   
-
+    this.load.image('globe', 'assets/vis/globe-1.png');
+    this.load.image('old-bridge', 'assets/vis/old-london-bridge.jpg');
 }
 
 
@@ -74,11 +72,13 @@ function create(){
     this.cameras.main.setBounds(0,0, this.game.canvas.width*4, this.game.canvas.height * 2);
     this.physics.world.setBounds(0,0, this.game.canvas.width*4, this.game.canvas.height * 2);
 
+    worldHeight = this.physics.world.bounds.height;
+//Backgrounds 
+this.add.image(720, this.physics.world.bounds.height-(48+118), 'globe').setScale(0.5,0.5);
+this.add.image((32*48) + 480, this.physics.world.bounds.height-(48+80), 'old-bridge');
 
 //AUDIO
-
     var bkgMusic = this.sound.add('bkg', {volume: 0.12, loop: true});
-
     bkgMusic.play();
 
     winSound = this.sound.add('win', {volume: 0.25});
@@ -86,9 +86,9 @@ function create(){
 //PLATFORMS
     platforms = this.physics.add.staticGroup();
   
-    platforms.create(32*4, this.physics.world.bounds.height-48,'ground').setScale(8, 3).refreshBody();   //1 
-    platforms.create(this.game.canvas.width-32*5, this.physics.world.bounds.height-48, 'ground').setScale(10,3).refreshBody(); //2
-    console.log("Physics world bounds " , this.physics.world.bounds.height);
+    platforms.create(32*24, this.physics.world.bounds.height-48,'ground').setScale(48, 3).refreshBody();   //1 
+    // platforms.create(this.game.canvas.width-32*5, this.physics.world.bounds.height-48, 'ground').setScale(10,3).refreshBody(); //2
+    console.log("Physics world bounds " , this.physics.world.bounds);
 
 //World bkg 
 
@@ -119,7 +119,7 @@ function create(){
        flipX: true,
    })
 //COIN  
-    var coin = this.physics.add.sprite(this.game.canvas.width-32, this.game.canvas.height - 175, 'coinPNG');
+    var coin = this.physics.add.sprite(this.game.canvas.width-32, this.physics.world.bounds.height - 175, 'coinPNG');
     coin.body.setAllowGravity(false);
     coin.body.setCircle(11,5,5);
 
@@ -131,11 +131,11 @@ function create(){
        repeat: -1
    }
  
-   player = this.physics.add.sprite(32, this.game.canvas.height - 150, 'cole');
+   player = this.physics.add.sprite(32, this.physics.world.bounds.height - 150, 'cole');
    player.setBounce(0.2);
-   player.setCollideWorldBounds(true);
+   player.setCollideWorldBounds(false);
    player.body.setSize(12,32);
-   player.body.setAllowGravity(false); //get rid of me once done testing
+//    player.body.setAllowGravity(false); //get rid of me once done testing
    player_anim = this.anims.create(player_config);
    player.anims.load('run');
    player.anims.play('run');
@@ -144,8 +144,7 @@ function create(){
    //camera to follow player
    this.cameras.main.startFollow(player, true, 0.1, 0.1, 15, 50);
 
-//Controls
-   arrows = this.input.keyboard.createCursorKeys();
+
 
 //Physics and collision
 
@@ -157,13 +156,17 @@ function create(){
    text = this.add.text(300, 50, "You Win");
    text.setVisible(false);
 
+
+//Desktop Controller
+    arrows = this.input.keyboard.createCursorKeys();
+
 //Mobile inputs
 
     game.input.addPointer();
     game.input.addPointer();  
     debugText = this.add.text(10, 10, 'input',{ font: '16px Courier', fill: '#00ff00' });
 
-    //duck it big jump button
+    //duck it big jump button that takes up most the screen
     var jumpButton = this.add.rectangle(this.game.canvas.width*0.65, this.game.canvas.height/2, this.game.canvas.width*0.75, this.game.canvas.height, 0x000000, 0);
     jumpButton.setInteractive();
     jumpButton.setScrollFactor(0);
@@ -188,7 +191,7 @@ function create(){
     rightArrow.setScrollFactor(0);
     arrowLine.setScrollFactor(0);
    
-   
+   //mobile button behavior
     leftButton.on('pointerdown', function(pointer){
         leftDown = true;
     });
@@ -196,7 +199,6 @@ function create(){
     rightButton.on('pointerdown', function(pointer){
         rightDown = true;
     })
-
 
     jumpButton.on('pointerdown', (pointer) => {
         console.log('jump button');
@@ -221,12 +223,7 @@ function render(){
 
 
 function update(){       
-
-    debugText.setText([
-        'mousePointer ' + this.input.mousePointer.isDown,
-        'pointer1 ' + this.input.pointer1.isDown,
-        'pointer2 ' + this.input.pointer2.isDown
-    ]);
+;
 
     if(this.input.mousePointer.isDown || this.input.pointer1.isDown || this.input.pointer2.isDown){       
        if(leftDown){
@@ -246,10 +243,6 @@ function update(){
         }
     }
 
-
-    
-    
-
   
 
 //Keyboard contorls
@@ -262,31 +255,30 @@ function update(){
     }else{
         player.setVelocityX(0);
     }
-<<<<<<< HEAD
 
 //Floaty controls for testing
-    if(arrows.up.isDown){
-        player.setVelocityY(-160);
-    }else if(arrows.down.isDown){
-        player.setVelocityY(160);
-    }else{
-        player.setVelocityY(0);
-    }
+    // if(arrows.up.isDown){
+    //     player.setVelocityY(-160);
+    // }else if(arrows.down.isDown){
+    //     player.setVelocityY(160);
+    // }else{
+    //     player.setVelocityY(0);
+    // }
 //end floaty
   
-    // if(arrows.up.isDown && player.body.touching.down){   
-    //     player.setVelocityY(-275);       
-    //     let timedEvent = this.time.delayedCall(500, jumpDecay)
-    // }
+    if(arrows.up.isDown && player.body.touching.down){   
+        player.setVelocityY(-275);       
+        let timedEvent = this.time.delayedCall(500, jumpDecay)
+    }
 
-    // if(!player.body.touching.down && arrows.up.isUp && player.body.velocity.y < 0){
-    //     jumpDecay();
-    // }
+    if(!player.body.touching.down && arrows.up.isUp && player.body.velocity.y < 0){
+        jumpDecay();
+    }
  
-    //testing
+    
 
 
-   // checkPlayerOOB(player);     
+   checkPlayerOOB(player);     
 }
 
 function jumpDecay(){    
@@ -295,7 +287,7 @@ function jumpDecay(){
 }
 
 function checkPlayerOOB(p){
-    if(p.getBounds().y > this.game.canvas.height){
+    if(p.getBounds().y > worldHeight){
         console.log("Player off screen");
         p.setPosition(playerSpawn.x, playerSpawn.y);
     }
@@ -307,12 +299,9 @@ function respawnPlayer(){
 
 function fireballTouch(){
     respawnPlayer();
-    var lossSound = this.sound.add('loss', {volume: 0.9});
-        lossSound.play();
 }
 
 function setWin(){
-
     if(!win){  
         text.setVisible(true);
         winSound.play();
